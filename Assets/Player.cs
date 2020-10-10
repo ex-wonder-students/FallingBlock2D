6ca554029db22bd77589103074e9fc56df9cc9ce
ex-wonder-students <72285976+ex-wonder-students@users.noreply.github.com>
2020-10-10 10:28:48 +0900

    プレイヤーを動かした。

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.3f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.3f, 0, 0);
+        }
+    }
+}
