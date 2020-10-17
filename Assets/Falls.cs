35fbc0e1d1b8d3586fba2959b521a277a647ad5e
ex-wonder-students <72285976+ex-wonder-students@users.noreply.github.com>
2020-10-17 10:31:43 +0900

    りんごをランダムで落とした

---
+++ Assets/Falls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falls : MonoBehaviour
+{
+    public GameObject falls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Falling", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Falling()
+    {
+        int x = Random.Range(-8, 8);
+        Instantiate(falls.gameObject, new Vector3(x, 4f, 0f), Quaternion.identity);
+    }
+}
+    
