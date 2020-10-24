9b1cc10a343cb59fc0cf8a93a37318892a12d2d0
ex-wonder-students <72285976+ex-wonder-students@users.noreply.github.com>
2020-10-24 10:10:42 +0900

    リンゴが消えるようになった

---
+++ Assets/FallsDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallsDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Kago")
+        {
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
