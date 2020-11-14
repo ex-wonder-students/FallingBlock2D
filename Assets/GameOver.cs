e8a40993f38bf29327048996e6619d58fe91fe79
ex-wonder-students <72285976+ex-wonder-students@users.noreply.github.com>
2020-11-14 10:24:16 +0900

    ゲームオーバーを実装した。60秒すると画面が切り替わる。

---
+++ Assets/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private float TimeLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeLimit = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLimit -= Time.deltaTime;
+
+        if(TimeLimit<=0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
